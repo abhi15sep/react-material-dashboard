8dd78f66dae7bd5d12d6813fe400669239709487
Akila Liyanage <mlakilaliyanage@gmail.com>
2020-05-24 20:10:59 +0530

    Updates

---
+++ precache-manifest.e7008de21cc414928f38891d046d1d5d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "638c1ebef7495db1a94fd4061b3e7948",
+    "url": "/react-material-dashboard/index.html"
+  },
+  {
+    "revision": "2cf34aa29c8478ef187e",
+    "url": "/react-material-dashboard/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "0509478249330ef11841",
+    "url": "/react-material-dashboard/static/js/2.8fbbb7f1.chunk.js"
+  },
+  {
+    "revision": "44e39e61fb6a0450f7c8014318774752",
+    "url": "/react-material-dashboard/static/js/2.8fbbb7f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2cf34aa29c8478ef187e",
+    "url": "/react-material-dashboard/static/js/main.8247cec0.chunk.js"
+  },
+  {
+    "revision": "83390a8f499218b45791",
+    "url": "/react-material-dashboard/static/js/runtime-main.1d837d33.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-material-dashboard/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/akilaliyanage/react-material-dashboard/precache-manifest.9345cdc6e8da0fef21d70aefc9d5523d.js"
+  "/react-material-dashboard/precache-manifest.e7008de21cc414928f38891d046d1d5d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/akilaliyanage/react-material-dashboard/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-material-dashboard/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
